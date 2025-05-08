f3286e8c20117b5f8714f04de4bfac325fe9ed71
GabrielGuevaraLopez <114413163+GabrielGuevaraLopez@users.noreply.github.com>
2025-05-08 23:35:42 +0200

    added basic voice recognition command

--- app/src/main/java/com/b1gbr0ther/MainActivity.kt
+++ app/src/main/java/com/b1gbr0ther/MainActivity.kt
@@ -5,19 +5,72 @@ import android.os.Bundle
 import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
+import android.speech.RecognizerIntent
+import android.speech.SpeechRecognizer
+import android.speech.RecognitionListener
+import android.widget.Toast
+import java.util.Locale
 
 
-class MainActivity : ComponentActivity() {
+class   MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
         val goToButton = findViewById<Button>(R.id.goToButton)
+        val speakButton = findViewById<Button>(R.id.btnSpeak)
 
         goToButton.setOnClickListener {
             val intent = Intent(this, HandGesturesActivity::class.java)
             startActivity(intent)
         }
+
+        speakButton.setOnClickListener {
+            startVoiceRecognition()
+        }
+    }
+
+    private fun startVoiceRecognition() {
+        val recognizer = SpeechRecognizer.createSpeechRecognizer(this)
+
+        val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH).apply {
+            putExtra(
+                RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM
+            )
+            putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault())
+        }
+
+
+        recognizer.setRecognitionListener(object : RecognitionListener {
+            override fun onResults(results: Bundle?) {
+                val matches = results?.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION)
+                val spokenText = matches?.firstOrNull()?.lowercase()
+
+                if (spokenText?.contains("hello big brother") == true) {
+                    Toast.makeText(
+                        this@MainActivity,
+                        "Big Brother always listens...",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+            }
+
+            override fun onError(error: Int) {
+                Toast.makeText(this@MainActivity, "Recognition error: $error", Toast.LENGTH_SHORT)
+                    .show()
+            }
+
+            override fun onReadyForSpeech(params: Bundle?) {}
+            override fun onBeginningOfSpeech() {}
+            override fun onRmsChanged(rmsdB: Float) {}
+            override fun onBufferReceived(buffer: ByteArray?) {}
+            override fun onEndOfSpeech() {}
+            override fun onPartialResults(partialResults: Bundle?) {}
+            override fun onEvent(eventType: Int, params: Bundle?) {}
+        })
+
+        recognizer.startListening(intent)
     }
 }
